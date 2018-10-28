5da66e8ab524f7d98a2b0b21fb31dd5e5d4d734b
schlenkibus <justus.scheil@parteimail.de>
2018-10-28 15:53:01 +0100

    wip

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -6,9 +6,9 @@ void Visualizer::update(NumberRecognition &r) {
         window.clear();
 
         auto colors = r.input.pixels;
-        for(auto y = 0; y < 28; y++) {
-            for(auto x = 0; x < 28; x++) {
-                image.setPixel(x, y, colors[y*28+x]);
+        for(unsigned int y = 0; y < 28; y++) {
+            for(unsigned int x = 0; x < 28; x++) {
+                image.setPixel(x, y, colors[y * 28 + x]);
             }
         }
 
